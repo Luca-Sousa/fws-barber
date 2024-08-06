0c17ecd4050b99a3c50ba5ae6c4efbf4db537af1
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-08-06 16:34:37 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -30,9 +33,29 @@ const Home = () => {
           />
         </div>
 
-        <div>
-          <h2 className="uppercase">Agendamentos</h2>
-        </div>
+        <h2 className="uppercase">Agendamentos</h2>
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="size-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+
+                <p className="text-sm">Barbearia FWS</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )

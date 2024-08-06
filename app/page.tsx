cc83c2fe5668b4a3f120e595431778ddce37c73b
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-08-06 16:08:10 -0300

    feat: add Header component add banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,41 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>test</Button>
+  return (
+    <div>
+      <Header />
+      <div className="space-y-6 p-5">
+        <div>
+          <h2 className="text-xl font-bold">Olá, Felipe</h2>
+          <p>Segunda-feira, 05 de agosto.</p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Input placeholder="Search" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative h-[150px] w-full overflow-hidden rounded-lg">
+          <Image
+            alt="Agende nos melhores com FWS Barber"
+            src="/banner-01.png"
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        <div>
+          <h2 className="uppercase">Agendamentos</h2>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home

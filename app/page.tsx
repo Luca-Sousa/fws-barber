67a9a4fe4c024ca94ab4663afc21faeab0bd2128
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-08-07 15:44:58 -0300

    feat: add PhoneItem component  and Footer to layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
-import { Card, CardContent } from "./_components/ui/card"
 import { db } from "./_lib/prisma"
 import BarberShopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
@@ -86,16 +85,6 @@ const Home = async () => {
           </div>
         </div>
       </div>
-
-      <footer>
-        <Card>
-          <CardContent className="px-5 py-6">
-            <p className="text-sm text-gray-400">
-              @ 2023 Copyright <span className="font-bold">FWS Barber</span>
-            </p>
-          </CardContent>
-        </Card>
-      </footer>
     </div>
   )
 }
